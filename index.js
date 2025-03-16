5461b6fc095fd7af6c4bb4cac50d360342a2412a
+ <moses.mutitu@student.moringaschool.com>
2025-03-16 18:55:35 +0300

    complete

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    // Split the string into words
+    return tutorial.split(' ')
+      // Capitalize the first letter of each word
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      // Join the words back into a single string
+      .join(' ');
+  });
+};
